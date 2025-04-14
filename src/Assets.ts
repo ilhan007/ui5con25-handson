5b9f3776f9d718134d8b2d8d2589bd37b0b614ab
Nayden Naydenov <nayden.naydenov@sap.com>
2025-04-14 16:51:44 +0300

    add assets and restore index page

--- src/Assets.ts
+++ src/Assets.ts
@@ -1,5 +1,8 @@
 import "@ui5/webcomponents-theming/dist/Assets.js"; // Theming
 
+import "@ui5/webcomponents/dist/Assets.js"; // Theming
+import "@ui5/webcomponents-fiori/dist/Assets.js"; // Theming
+
 // own ui5con package assets
 import "./generated/json-imports/Themes.js";
 import "./generated/json-imports/i18n.js";
