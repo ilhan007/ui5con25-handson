c5945fc890be0320910dcf49f0efce838b7bf18a
ilhan007 <ilhan.myumyun@sap.com>
2025-04-27 22:24:31 +0300

    chore: update landing page

---
+++ src/bundle.esm.ts
@@ -0,0 +1,9 @@
+// Enable additional themes and i18n texts
+import "./Assets.js";
+
+// Import your web components here from the dist/ directory
+import "./Chat.js";
+import "./ChatBubble.js";
+import "./ChatLoading.js";
+
+import "@ui5/webcomponents/dist/Button.js";
